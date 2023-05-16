52a760e66d769b702f901c8b8db4926145cf542c
Pyon18Pyon <ayasayshi@gmail.com>
2023-05-16 23:11:44 +1000

    feat: update my github address

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -4,7 +4,7 @@ import './Sections.css';
 function Sections() {
   return (
     <div>
-      <section id="home"><a href="https://github.com/Pyon18Pyon" target="_blank">Ayako GitHub</a></section>
+      <section id="home"><a href="https://github.com/PyonAU" target="_blank">Ayako GitHub</a></section>
       <section id="about"><h1>Learn More About Me</h1></section>
       <section id="skills"><h1>These Are My Strengths</h1></section>
       <section id="projects"><h1>These Are My Results</h1></section>
